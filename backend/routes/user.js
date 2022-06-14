c6a4462c76da09d3e9bd0008b390b5ccc8cbbbf9
Rocky.C <cloki0610@gmail.com>
2022-06-14 18:46:49 +0000

    Remove .js file and update git ignore

--- backend/routes/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const userController = require("../controllers/user");
-const router = express.Router();
-router.get("/all-data", userController.getAllData);
-router.post("/new-data", userController.postNewData);
-exports.default = router;
